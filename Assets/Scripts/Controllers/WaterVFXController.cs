113283c95a16a9fb8dcb372eb3821ed4a8f13805
ardicDemirol <ardc.dmrol@gmail.com>
2024-03-21 16:53:30 +0300

    particle collision done

--- Assets/Scripts/Controllers/WaterVFXController.cs
+++ Assets/Scripts/Controllers/WaterVFXController.cs
@@ -2,14 +2,49 @@ using UnityEngine;
 
 public class WaterVFXController : MonoBehaviour
 {
+    [SerializeField] private float minCollisionTime = 5f;
+    private float _currentCollisionTime;
     private const string DIRT_VFX_TAG = "Dirt";
+
+    private GameObject _lastDirtObject;
+
 
     private void OnParticleCollision(GameObject other)
     {
         if (other.CompareTag(DIRT_VFX_TAG))
         {
-            Debug.Log("Water VFX Collided with Dirt VFX");
-            //other.GetComponent<ParticleSystem>().Stop();
+            if (_lastDirtObject != other)
+            {
+                _lastDirtObject = other;
+                _currentCollisionTime = 0;
+            }
+            if (Timer())
+            {
+                if (other.TryGetComponent(out ParticleSystem particleSystem))
+                {
+                    particleSystem.Stop();
+                }
+                else if (other.TryGetComponent(out SpriteRenderer spriteRenderer))
+                {
+                    spriteRenderer.enabled = false;
+                }
+            }
         }
     }
+
+    private bool Timer()
+    {
+        if (_currentCollisionTime >= minCollisionTime)
+        {
+            _currentCollisionTime = 0;
+            return true;
+        }
+        else
+        {
+            _currentCollisionTime += 1 * Time.maximumParticleDeltaTime;
+            return false;
+        }
+    }
+
+
 }
